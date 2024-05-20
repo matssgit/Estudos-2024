e660502d8edd2cf1eb7ae9263a338480e8650c5f
Matheus Santana <100173811+matssgit@users.noreply.github.com>
2024-05-20 16:43:51 -0300

    end course

--- JavaScript/aula16ex/ex018/script.js
+++ JavaScript/aula16ex/ex018/script.js
@@ -21,10 +21,37 @@ function inLista(n, l) {
 
 function add() {
     if(isNumero(num.value) && !inLista(num.value, valores)) {
-        
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado.`
+        lista.appendChild(item)
     } else {
         alert('Valor inválido ou já encontrado na lista.')
     }
-
+    num.value = ''
+    num.focus()
     
+}
+
+function finalizar() {
+    if(valores.length == 0) {
+        alert('Adicione valores antes de finalizar')
+    } else {
+        let tot = valores.length
+        valores.sort()
+        let menor = valores[0]
+        let maior = valores[tot-1]
+        let soma = 0
+        let media = 0
+        for(let pos in valores) {
+            soma += valores[pos]
+            media = soma / tot
+        }
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo, temos ${tot} números cadastrados.</p>`
+        res.innerHTML += `<p>O menor valor informado foi ${menor}</p>`
+        res.innerHTML += `<p>O maior valor informado foi ${maior}</p>`
+        res.innerHTML += `<p>A soma dos valores é ${soma}</p>`
+        res.innerHTML += `<p>A média dos valores é ${media}</p>`
+    }
 }
