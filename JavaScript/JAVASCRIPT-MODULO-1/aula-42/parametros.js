523c714041ea244ce1f2297391755b025b48930d
Matheus Santana <100173811+matssgit@users.noreply.github.com>
2024-05-27 22:07:18 -0300

    functions

---
+++ JavaScript/JAVASCRIPT-MODULO-1/aula-42/parametros.js
@@ -0,0 +1,18 @@
+function dobro(x) {
+    console.log(`O dobro de ${x} é ${x*2}`)
+}
+
+// dobro(23)
+
+function dizerOla(nome = "mundo"){
+    alert(`Olá ${nome}`)
+}
+
+// dizerOla("Matheus")
+// dizerOla()
+
+function soma(a, b, c , d, e) {
+    alert(`O resultado da soma é ${a + b + c + d + e}`)
+}
+
+soma(7, 6, 4, 1, 9)
