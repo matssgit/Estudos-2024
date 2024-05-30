03104305e99d301f23c0efa00ddd50ad5946d2a4
Matheus Santana <100173811+matssgit@users.noreply.github.com>
2024-05-30 14:00:45 -0300

    retorno de funcao

--- JavaScript/JAVASCRIPT-MODULO-1/aula-42/parametros.js
+++ JavaScript/JAVASCRIPT-MODULO-1/aula-42/parametros.js
@@ -37,3 +37,23 @@ function novoUsuario(nome, tipo="admin", email, senha){
 }
 criarUsuario("Matheus", "matheus@email.com", "1234")
 novoUsuario("Matheus", "admin", "matheus@email.com", "1234")
+
+// Ao criarmos uma função com muitos parâmetros,
+// uma boa prática é substituí-los por um objeto
+function parametrosDoJeitoErrado(nome, telefone, endereco, aniversario, email, senha) {
+    // ...
+  }
+  function parametrosDoJeitoCerto(usuario) {
+    // ...
+  }
+  // Além de facilitar na chamada da função, a ordem dos parâmetros se torna irrelevante
+  parametrosDoJeitoErrado("nome", "telefone", "endereco", "aniversario", "email", "senha")
+  const dadosDoUsuario = {
+    nome: "",
+    telefone: "",
+    email: "",
+    senha: "",
+    aniversario: "",
+    endereco: ""
+  }
+  parametrosDoJeitoCerto(dadosDoUsuario)
---
+++ JavaScript/JAVASCRIPT-MODULO-1/aula-43/retorno.js
@@ -0,0 +1,51 @@
+function calcularMedia(a, b) {
+    const media = (a +b) / 2
+    return media
+}
+
+const resultado = calcularMedia(7, 2)
+
+//console.log(resultado)
+
+function criarProduto(nome, preco) {
+    const produto = {
+        nome, // nome: nome
+        preco, //preco: preco
+        estoque: 1
+    }
+    return produto
+}
+
+// console.log(criarProduto("Macbook M1", 4000))
+
+function areaRetangular(base, altura) {
+    return base* altura
+}
+
+function areaQuadrada(lado) {
+    return areaRetangular(lado, lado)
+}
+
+// console.log(areaRetangular(3,5))
+
+// console.log(areaQuadrada(9))
+
+function ola() {
+    let texto = "..."
+    return texto
+    texto = "Olá, mundo!"
+    console.log(texto)
+}
+
+console.log(ola())
+
+function maioridade(idade){
+    if (idade > 18){
+        return "Maior de idade"
+    } else {
+        return "Menor de idade"
+    }
+}
+
+console.log(maioridade(29))
+console.log(maioridade(13))
