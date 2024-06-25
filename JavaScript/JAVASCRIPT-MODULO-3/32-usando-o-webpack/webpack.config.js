79247ba0b90a26034054f909ac73248f9886673e
Matheus Santana <100173811+matssgit@users.noreply.github.com>
2024-06-25 19:49:02 -0300

    webpack

---
+++ JavaScript/JAVASCRIPT-MODULO-3/32-usando-o-webpack/webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path')
+
+module.exports = {
+    entry: {
+        main: './src/index.js',
+    },
+    mode: 'development',
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader']
+        }]
+    }
+}
